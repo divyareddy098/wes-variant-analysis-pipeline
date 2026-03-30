fe61dad7330a0d35c7695cb35d84547a9aa3d16f
divyareddy098 <divyareddypeddi@gmail.com>
2026-03-29 23:33:41 -0400

    Create annotation.sh

---
+++ scripts/annotation.sh
@@ -0,0 +1,12 @@
+# Purpose: Annotate variants using bcftools
+
+INPUT_VCF="results/filtered_variants.vcf"
+OUTPUT_VCF="results/annotated_variants.vcf"
+
+# annotation 
+bcftools annotate \
+  -x ID \
+  $INPUT_VCF \
+  -o $OUTPUT_VCF
+
+echo "Annotation complete: $OUTPUT_VCF"
