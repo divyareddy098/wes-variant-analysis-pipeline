1344550132f042e1e52fe5d42359487bc0f45fa4
divyareddy098 <divyareddypeddi@gmail.com>
2026-04-29 17:25:24 -0400

    Delete scripts/variant_calling.sh

--- scripts/variant_calling.sh
+++
@@ -1,9 +0,0 @@
-# Purpose: Call variants using bcftools
-
-REFERENCE="data/reference/genome.fa"
-BAM="results/aligned.bam"
-VCF="results/raw_variants.vcf"
-
-bcftools mpileup -f $REFERENCE $BAM | bcftools call -mv -o $VCF
-
-echo "Variant calling complete: $VCF"
