e7bee410485993bf8e46e614f0acec0705a3191d
divyareddy098 <divyareddypeddi@gmail.com>
2026-04-29 17:25:01 -0400

    Delete scripts/annotation.sh

--- scripts/annotation.sh
+++
@@ -1,8 +0,0 @@
-# Purpose: Annotate variants using bcftools
-
-INPUT_VCF="results/filtered_variants.vcf"
-OUTPUT_VCF="results/annotated_variants.vcf"
-
-bcftools annotate -x ID $INPUT_VCF -o $OUTPUT_VCF
-
-echo "Annotation complete: $OUTPUT_VCF"
